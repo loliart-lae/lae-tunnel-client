71352ec565f954349ebbb8598a40fc723707254b
Bing_Yanchi <Bing_Yanchi@outlook.com>
2021-10-27 23:18:38 +0800

    新增 token 获取隧道

--- lae-tunnel.py
+++ lae-tunnel.py
@@ -5,7 +5,7 @@
 from threading import Thread
 from queue import Queue
 from win10toast import ToastNotifier
-import time, json, argparse
+import time, json, argparse, requests
 import frpgo
 
 # 通信部分    
@@ -15,9 +15,42 @@ o = Queue()
 # Windows 通知
 toaster = ToastNotifier()
 
+# 配置部分
+get_tunnels_url = "http://lightart.top/api/v1/_tunnels/"
+
+# 获取隧道列表
+def printTunnel():
+    #try:
+        r = requests.get(get_tunnels_url + "?api_token={}".format(token), verify=False)
+
+        if (r.status_code == 200):
+            # 正确获取
+            result = json.loads(r.text)
+
+            #print(result)
+            if (result['status'] == 0):
+                print("[WARN] 请求出错, 或许是请求冷却或服务器故障")
+                return False
+            
+            table_header = "隧道 ID\t名称\t协议\t本地地址\t在线状态\t服务器 ID\t项目 ID\t最后在线"
+            table_format = "{id}\t{name}\t{protocol}\t{local_address}\t{status}\t{server_id}\t{project_id}\t{ping}"
+            
+            print(table_header)
+
+            for line in result['data']:
+                print(table_format.format(id=line['id'], name=line['name'], protocol=line['protocol'], local_address=line['local_address'], status=line['status'], server_id=line['server_id'], project_id=line['project_id'], ping=line['ping']))
+            return True
+        else:
+            # 验证失败
+            return False
+    #except:
+        # 本地错误
+    #    print("[WARN] 请求出错, 或许是请求冷却或程序已过期")
+    #    return False
+
 # Debug 模式
-Debug = False
-if (Debug):
+Debug = True
+if (not Debug):
     print("========================================================================================================")
     print("")
     print("████████╗██╗   ██╗███╗   ██╗███╗   ██╗███████╗██╗          ██████╗██╗     ██╗███████╗███╗   ██╗████████╗")
@@ -27,16 +60,30 @@ if (Debug):
     print("   ██║   ╚██████╔╝██║ ╚████║██║ ╚████║███████╗███████╗    ╚██████╗███████╗██║███████╗██║ ╚████║   ██║   ")
     print("   ╚═╝    ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═══╝╚══════╝╚══════╝     ╚═════╝╚══════╝╚═╝╚══════╝╚═╝  ╚═══╝   ╚═╝   ")
     print("                                                                                                        ")
-    print("[DEBUG] 嘿，你目前处于调试模式，更多记录将会展示")
     print("========================================================================================================")
+    print("")
 
 if __name__ == "__main__":
-    
+    # 检查参数
     parser = argparse.ArgumentParser(description='Light App Engine Tunnel Client.')
     parser.add_argument('-a', '--token', help='User Token')
-    parser.add_argument('-t', '--tunnel', help='Tunnel ID (Use , split)', type=int)
+    parser.add_argument('-t', '--tunnel', help='Tunnel ID (Use , split)')
     args = parser.parse_args()
-    ip = args.address or '0.0.0.0'
-    port = args.port or 8080
 
-    print(ip, port)
+    token = args.token
+    tunnel = args.tunnel
+
+    # 最默认的模式
+    if (token == None and tunnel == None):
+
+        while (1):
+
+            token = input("[INFO] 输入你的 Token: ")
+
+            if (printTunnel()):
+                break
+            else:
+                print("[WARN] Token 验证失败, 请重试...")
+
+        
+        
