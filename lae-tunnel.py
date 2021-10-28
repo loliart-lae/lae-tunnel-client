083c49af695e7965f8b41658cf9150964c1ff67a
Bing_Yanchi <Bing_Yanchi@outlook.com>
2021-10-28 20:52:21 +0800

    新增 获取项目 & 节点

--- lae-tunnel.py
+++ lae-tunnel.py
@@ -16,12 +16,46 @@ o = Queue()
 toaster = ToastNotifier()
 
 # 配置部分
-get_tunnels_url = "http://lightart.top/api/v1/_tunnels/"
+get_tunnels_url = "http://lightart.top/api/v1/_tunnels"
+get_project_url = "http://lightart.top/api/_projects"
+get_server_url = "http://lightart.top/api/v1/_tunnels/create"
+
+# 变量部分
+project = {}
+server = {}
+
+def getUserInfo():
+    r = requests.get(get_project_url + "?api_token={}".format(token))
+
+    if (r.status_code != 200): return False
+
+    result = json.loads(r.text)
+
+    if (result['status'] == 0):
+        print("[WARN] 请求出错, 或许是请求冷却或服务器故障")
+        return False
+
+    for line in result['data']:
+        project[line['project']['id']] = line['project']['name']
+    
+    r = requests.get(get_server_url + "?api_token={}".format(token))
+
+    if (r.status_code != 200): return False
+
+    result = json.loads(r.text)
+
+    if (result['status'] == 0):
+        print("[WARN] 请求出错, 或许是请求冷却或服务器故障")
+        return False
+    
+    for line in result['data']:
+        server[line['id']] = line['name']
+
 
 # 获取隧道列表
 def printTunnel():
     #try:
-        r = requests.get(get_tunnels_url + "?api_token={}".format(token), verify=False)
+        r = requests.get(get_tunnels_url + "?api_token={}".format(token))
 
         if (r.status_code == 200):
             # 正确获取
@@ -42,6 +76,8 @@ def printTunnel():
             return True
         else:
             # 验证失败
+            print("错误代码: " , r.status_code)
+            print(r.text)
             return False
     #except:
         # 本地错误
@@ -49,7 +85,7 @@ def printTunnel():
     #    return False
 
 # Debug 模式
-Debug = True
+Debug = False
 if (not Debug):
     print("========================================================================================================")
     print("")
@@ -80,10 +116,19 @@ if __name__ == "__main__":
 
             token = input("[INFO] 输入你的 Token: ")
 
+            getUserInfo()
+
+            print(project)
+            print(server)
+
             if (printTunnel()):
                 break
             else:
                 print("[WARN] Token 验证失败, 请重试...")
+        
+        while(1):
+
+            tunnel = input("[INFO] 输入你要连接的隧道: ")
 
         
         
