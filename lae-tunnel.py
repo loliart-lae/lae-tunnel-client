e287b9f738fa578480511a5f8bc205467e225f88
Bing_Yanchi <Bing_Yanchi@outlook.com>
2021-10-30 20:13:36 +0800

    新增 下载配置文件预备

--- lae-tunnel.py
+++ lae-tunnel.py
@@ -19,15 +19,18 @@ o = Queue()
 get_tunnels_url = "http://lightart.top/api/v1/_tunnels"
 get_project_url = "http://lightart.top/api/_projects"
 get_server_url = "http://lightart.top/api/v1/_tunnels/create"
+get_config_url = " https://lightart.top/api/v1/_tunnels/"
 
 # 变量部分
 project = {}
 server = {}
+tunnel = {}
+config = {}
 
 # 调试模式
 Debug = False
 
-def sendrequest(url):
+def sendrequest(url, original):
 
     r = requests.get(url)
 
@@ -37,6 +40,9 @@ def sendrequest(url):
             if (Debug):
                 print("[DEBUG] 有一条请求状态码错误: {} - {}".format(str(r.status_code), url))
             continue
+
+        if (original == True):
+            return r.text
 
         result = json.loads(r.text)
 
@@ -52,14 +58,14 @@ def sendrequest(url):
 
 def getUserInfo():
     
-    result = sendrequest(get_project_url + "?api_token={}".format(token))
+    result = sendrequest(get_project_url + "?api_token={}".format(token), False)
 
     if (result == "error"): return False
 
     for line in result:
         project[line['project']['id']] = line['project']['name']
     
-    result = sendrequest(get_server_url + "?api_token={}".format(token))
+    result = sendrequest(get_server_url + "?api_token={}".format(token), False)
     
     if (result == "error"): return False
     
@@ -69,22 +75,43 @@ def getUserInfo():
 
 # 获取隧道列表
 def printTunnel():
-    result = sendrequest(get_tunnels_url + "?api_token={}".format(token))
+    result = sendrequest(get_tunnels_url + "?api_token={}".format(token), False)
 
     if (result == "error"): return False
+    
+    table_format = "%-5s\t%-10s\t%-5s\t%-10s\t%-15s\t%-12s\t%-10s"
             
-    table_header = "隧道 ID\t名称\t协议\t本地地址\t在线状态\t服务器\t项目\t最后在线"
-    table_format = "{id}\t{name}\t{protocol}\t{local_address}\t{status}\t{server_id}\t{project_id}\t{ping}"
-            
-    print(table_header)
+    print(table_format%("隧道 ID","名称","协议","本地地址","服务器","项目","最后在线"))
 
     for line in result:
-        if (line['status'] == 1): status_name = "在线"
-        else: status_name = "离线"
         server_name = server[line['server_id']]
         project_name = project[line['project_id']]
-        print(table_format.format(id=line['id'], name=line['name'], protocol=line['protocol'], local_address=line['local_address'], status=status_name, server_id=server_name, project_id=project_name, ping=line['ping']))
+        print(table_format%(line['id'],line['name'],line['protocol'],line['local_address'],server_name,project_name,line['ping']))
+        tunnel[line['id']] = line['project_id']
+
     return True
+
+# 下载配置文件
+def get_config(id):
+    result = sendrequest(get_config_url + id + "?api_token={}".format(token), True)
+
+    if (result == "error"): return False
+
+
+
+# 启动隧道
+def runTunnel(tunnels):
+
+    success = 0
+
+    for tunnel_id in tunnels:
+        if (tunnel_id in tunnel.keys()):
+
+            success += 1
+    
+    if (success == 0): return False
+    else: return True
+
 
 # Debug 模式
 if (not Debug):
@@ -108,19 +135,16 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     token = args.token
-    tunnel = args.tunnel
+    arg_tunnel = args.tunnel.split(',')
 
     # 最默认的模式
-    if (token == None and tunnel == None):
+    if (token == None and arg_tunnel == None):
 
         while (1):
 
             token = input("[INFO] 输入你的 Token: ")
 
             getUserInfo()
-
-            print(project)
-            print(server)
 
             if (printTunnel()):
                 break
@@ -129,7 +153,13 @@ if __name__ == "__main__":
         
         while(1):
 
-            tunnel = input("[INFO] 输入你要连接的隧道: ")
+            arg_tunnel = input("[INFO] 输入你要连接的隧道 ID (直接回车将连接所有隧道): ").split(',')
 
+            if(runTunnel(arg_tunnel)):
+                break
+            else:
+                print("[WARN] 所有输入的隧道均不存在, 请重新输入...")
         
-        
+
+
+
